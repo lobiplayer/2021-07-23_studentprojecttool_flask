2a3dfd28686a48d9194d47b126c0ccfc4cf33145
Robin Mohan <76623233+lobiplayer@users.noreply.github.com>
2021-07-27 21:50:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 app = Flask('GARDONE', root_path=web_dir)
