7a5cc43ea7956597d244d70eb1c809d2333b8d10
Lobiplayer <jrobinmohan@gmail.com>
2021-07-27 18:19:47 +0800

    postgresql migration done

--- api/views.py
+++ api/views.py
@@ -108,3 +108,19 @@ def deadline_homepage():
     print(deadline_homepage)
 #this will be send to the client (this will be the response for the get request)
     return jsonify({'deadline_homepage' : deadline_homepage})
+
+@main.route('/test', methods=['GET'])
+def test():
+
+
+# geting the Todo records from the database. here we have to make where todo.user_id == currentuser
+    todo_list = Todo.query.all()
+    #this is the list that will be send to the react app:
+    todos = []
+
+#every todo from the database will be formatted in a dictionary, this will be appended to the todos list (2 lines back)
+    for todo in todo_list:
+        todos.append({'id':todo.id ,'todo_text': todo.todo_text, 'is_done': todo.is_done, 'deadline_id': todo.deadline_id, 'user_id': user_id['user_id'], 'created_at': todo.created_at, 'updated_at': todo.updated_at})
+
+#this will be send to the client (this will be the response for the get request)
+    return jsonify({'todos' : todos})
