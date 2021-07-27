f9d5163ea0a2e0487dc25e214b31328e67ec0078
Lobiplayer <jrobinmohan@gmail.com>
2021-07-27 15:40:22 +0800

    modified for the user_id token

--- api/views.py
+++ api/views.py
@@ -42,18 +42,31 @@ def add_deadline():
 
     deadline_data = request.get_json()
 
-    new_deadline = Deadline(description=deadline_data['description'], date=deadline_data['date'], subject=deadline_data['subject'], user_id=random.randint(1, 1000000), created_at = datetime.datetime.now(), updated_at= None)
+    new_deadline = Deadline(description=deadline_data['description'], date=deadline_data['date'], subject=deadline_data['subject'], user_id=deadline_data['user_id'], created_at = datetime.datetime.now(), updated_at= None)
 
     db.session.add(new_deadline)
     db.session.commit()
 
     return 'Done', 201
 
-@main.route('/deadlines', methods=['GET'])
-def deadlines():
-    deadline_list = Deadline.query.all()
-    deadlines = []
+# @main.route('/deadlines', methods=['GET'])
+# def deadlines():
+#     deadline_list = Deadline.query.all()
+#     deadlines = []
 
+#     for deadline in deadline_list:
+#         deadlines.append({'id':deadline.id ,'description': deadline.description, 'deadline_date': deadline.date, 'subject': deadline.subject, 'user_id': deadline.user_id, 'created_at': deadline.created_at, 'updated_at': deadline.updated_at})
+
+#     return jsonify({'deadlines' : deadlines})
+
+@main.route('/deadlines', methods=['POST'])
+def deadlines():
+
+    user_data = request.get_json()
+# geting the Todo records from the database. here we have to make where todo.user_id == currentuser
+    deadline_list = Deadline.query.filter_by(user_id = user_data['user_id'])
+    deadlines = []
+    #this is the list that will be send to the react app:
     for deadline in deadline_list:
         deadlines.append({'id':deadline.id ,'description': deadline.description, 'deadline_date': deadline.date, 'subject': deadline.subject, 'user_id': deadline.user_id, 'created_at': deadline.created_at, 'updated_at': deadline.updated_at})
 
